0edb72417910e06916f77d43525be72d1626951b
samluiz <samuelluizrl@outlook.com>
2023-02-16 17:15:23 -0300

    test: populate method

--- src/main/java/ibm/samuelluiz/interestrateapi/models/dtos/MonthlyInterestRateList.java
+++ src/main/java/ibm/samuelluiz/interestrateapi/models/dtos/MonthlyInterestRateList.java
@@ -2,11 +2,13 @@ package ibm.samuelluiz.interestrateapi.models.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import ibm.samuelluiz.interestrateapi.models.MonthlyInterestRate;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Collection;
 
 @Data
+@AllArgsConstructor
 public class MonthlyInterestRateList {
     @JsonProperty("value")
     private Collection<MonthlyInterestRate> results;
