a6a3440569f8c8dabb6d8ad7892ad40549f3478e
samluiz <samuelluizrl@outlook.com>
2023-02-16 11:41:09 -0300

    build: add dependencies and initialize project

---
+++ src/main/java/ibm/samuelluiz/interestrateapi/InterestrateapiApplication.java
@@ -0,0 +1,13 @@
+package ibm.samuelluiz.interestrateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterestrateapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterestrateapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibm/samuelluiz/interestrateapi/InterestrateapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ibm.samuelluiz.interestrateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterestrateapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
