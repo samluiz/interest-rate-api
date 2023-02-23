604a5e15b548863662d8514cb10e44feaf7fb090
Samuel Luiz Rego Leal <samuelluiz@ibm.com>
2023-02-23 18:03:05 -0300

    refactor: create dto and change json properties to snake case

--- src/main/java/ibm/samuelluiz/interestrateapi/models/MonthlyInterestRate.java
+++ src/main/java/ibm/samuelluiz/interestrateapi/models/MonthlyInterestRate.java
@@ -1,6 +1,7 @@
 package ibm.samuelluiz.interestrateapi.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import ibm.samuelluiz.interestrateapi.models.dtos.MonthlyInterestRateDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,34 +27,34 @@ public class MonthlyInterestRate extends RepresentationModel<MonthlyInterestRate
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(length = 36)
     private String _uuid;
-    @JsonProperty("Modalidade")
-    @NotEmpty(message = "O campo 'Modalidade' não pode ser vazio.")
+    @JsonProperty("modalidade")
+    @NotEmpty(message = "O campo 'modalidade' não pode ser vazio.")
     private String _modality;
-    @JsonProperty("Posicao")
-    @NotNull(message = "O campo 'Posicao' não pode ser nulo.")
+    @JsonProperty("posicao")
+    @NotNull(message = "O campo 'posicao' não pode ser nulo.")
     private Integer _position;
-    @JsonProperty("InstituicaoFinanceira")
-    @NotEmpty(message = "O campo 'InstituicaoFinanceira' não pode ser vazio.")
+    @JsonProperty("instituicao_financeira")
+    @NotEmpty(message = "O campo 'instituicao_financeira' não pode ser vazio.")
     private String _financialInstitution;
-    @JsonProperty("TaxaJurosAoMes")
-    @NotNull(message = "O campo 'TaxaJurosAoMes' não pode ser vazio.")
+    @JsonProperty("taxa_juros_ao_mes")
+    @NotNull(message = "O campo 'taxa_juros_ao_mes' não pode ser vazio.")
     private Double _interestRateByMonth;
-    @JsonProperty("TaxaJurosAoAno")
-    @NotNull(message = "O campo 'TaxaJurosAoAno' não pode ser vazio.")
+    @JsonProperty("taxa_juros_ao_ano")
+    @NotNull(message = "O campo 'taxa_juros_ao_ano' não pode ser vazio.")
     private Double _interestRateByYear;
-    @JsonProperty("cnpj8")
-    @NotEmpty(message = "O campo 'cnpj8' não pode ser vazio.")
+    @JsonProperty("cnpj_8")
+    @NotEmpty(message = "O campo 'cnpj_8' não pode ser vazio.")
     @Size(min = 8, max = 8, message = "O campo 'cnpj8' deve ter 8 dígitos.")
     @Column(length = 8)
     private String _eightDigitsCnpj;
-    @JsonProperty("Mes")
+    @JsonProperty("mes")
     @NotEmpty
-    @Size(min = 8, max = 8, message = "O campo 'Mes' deve ter 8 dígitos. ex: Ago-2003")
+    @Size(min = 8, max = 8, message = "O campo 'mes' deve ter 8 dígitos. ex: Ago-2003")
     @Column(length = 8)
     private String _month;
-    @JsonProperty("anoMes")
+    @JsonProperty("ano_mes")
     @NotEmpty
-    @Size(min = 7, max = 7, message = "O campo 'anoMes' deve ter 7 dígitos. ex: 2003-08")
+    @Size(min = 7, max = 7, message = "O campo 'ano_mes' deve ter 7 dígitos. ex: 2003-08")
     @Column(length = 7)
     private String _yearMonth;
 }
