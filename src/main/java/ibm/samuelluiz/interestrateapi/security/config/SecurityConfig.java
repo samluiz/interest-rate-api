a9379202b79a182b6afc487ad628e0c5274e1610
Samuel Luiz Rego Leal <samuelluiz@ibm.com>
2023-02-24 11:37:50 -0300

    feat: implement oauth2 with google cloud provider

---
+++ src/main/java/ibm/samuelluiz/interestrateapi/security/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package ibm.samuelluiz.interestrateapi.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+
+        return http.build();
+    }
+}
