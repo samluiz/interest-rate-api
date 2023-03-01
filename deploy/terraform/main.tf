d19fbd4f5fc6419fa4e974dab7230b44da25ee60
samluiz <samuelluizrl@outlook.com>
2023-03-01 19:47:16 -0300

    deploy: configure terraform

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,26 @@
+resource "google_compute_subnetwork" "public-api-subnet" {
+  ip_cidr_range = "10.0.0.0/16"
+  name          = "public-api-subnetwork"
+  network       = google_compute_network.api-network.name
+  depends_on = [google_compute_network.api-network]
+}
+
+resource "google_compute_network" "api-network" {
+  name = "api-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "api-firewall"
+  network = google_compute_network.api-network.name
+
+  allow {
+    protocol = "all"
+  }
+
+  priority = 1000
+  direction = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+
+  source_tags = ["web"]
+}
---
+++ deploy/terraform/provider.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("C:/.gcp/interestrateapi-credentials.json")
+  project = "interest-rate-api"
+  region  = "us-east1"
+  zone    = "us-east1-b"
+}
+
+resource "google_compute_instance" "api-server" {
+  name = "interest-rate-api-server"
+  machine_type = "e2-micro"
+  can_ip_forward = "true"
+  boot_disk {
+    initialize_params {
+      type = "pd-standard"
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+  metadata = {
+    startup-script = file("script.tpl")
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.public-api-subnet.name
+    access_config {}
+    }
+  depends_on = [google_compute_subnetwork.public-api-subnet]
+  }
